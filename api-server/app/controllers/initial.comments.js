22941c5ddb5b220aeda437a771f81217a83f2468
Sorob Raissi <cybrobo@gmail.com>
2018-10-10 02:32:09 -0700

    Styled comment buttons more.

--- api-server/app/controllers/initial.comments.js
+++ api-server/app/controllers/initial.comments.js
@@ -19,5 +19,26 @@ module.exports = InitialComments = [
         timestamp: "Sat Jan 22 1974",
         avatar: "😘",
         likes: 3159
+    },
+    {
+        name: "Johnny Doe",
+        comment: "1st! This is an awesome comment! It is the mostest comment of all the comments. I disagree with everyone on how great their comments are and prefer mine over others.",
+        timestamp: "Fri Sept 11 1984",
+        avatar: "😊",
+        likes: 51
+    },
+    {
+        name: "Jackey Jillara",
+        comment: "There's some neat comments here. Way better than youtubes...",
+        timestamp: "Mon Aug 9 1962",
+        avatar: "😫",
+        likes: 207
+    },
+    {
+        name: "Downey Texler",
+        comment: "I like fried fish and chips becus ahm brihtish shashasha!!!",
+        timestamp: "Sat Jan 22 1974",
+        avatar: "😘",
+        likes: 3159
     }
 ]
